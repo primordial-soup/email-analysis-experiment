0f8bfa204b59551d3cff11701d4a6af5081828f0
Zakariyya Mughal <zaki.mughal@gmail.com>
2015-05-18 17:54:41 -0500

    add a helper for plot files

--- lib/EnronBerkeley/ProjectHelper.pm
+++ lib/EnronBerkeley/ProjectHelper.pm
@@ -15,9 +15,21 @@ sub output_directory_path {
 	$self->top_level_directory_path->child( qw(output) );
 }
 
+sub plot_directory_path {
+	my ($self) = @_;
+	defined $ENV{ENRON_PLOT_PATH}
+		?  path($ENV{ENRON_PLOT_PATH})
+		: $self->top_level_directory_path->child( qw(gfx) );
+}
+
 sub output_file {
 	my ($self, @children) = @_;
 	$self->output_directory_path->child( @children );
+}
+
+sub plot_file {
+	my ($self, @children) = @_;
+	$self->plot_directory_path->child( @children );
 }
 
 1;
